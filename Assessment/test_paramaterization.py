d1381873247f154d0664d5f8b07b655540c42ae4
samrat <samratghimire7@gmail.com>
2023-10-25 04:05:12 +0545

    this is some changes

--- Assessment/test_paramaterization.py
+++ Assessment/test_paramaterization.py
@@ -1,91 +1,56 @@
+
 import pytest
 import requests
 
-Base_URL = "https://practice.expandtesting.com/notes/api/api-docs/#/"
+# Fixture to initialize the API client
+@pytest.fixture
+def api_client():
+    base_url = "https://practice.expandtesting.com/notes/api/api-docs/#/"
+    return requests.Session()
 
-# Smoke test for creating a new note
-@pytest.mark.smoke
-def test_create_note():
-    response = requests.post(
-        Base_URL + "/notes",
-        json={"title": "Test note", "content": "This is a test note."},
-    )
+# Fixture to create test notes
+def test_create_note(api_client, create_test_category, base_url="https://practice.expandtesting.com/notes/api/api-docs/#/"):
+    note_data = {
+        "title": "New Note",
+        "content": "This is a new note.",
+        "category_id": create_test_category
+    }
+    response = api_client.post(f"{base_url}/notes", json=note_data)
     assert response.status_code == 200
 
-#Categorizing notes into different category
-@pytest.mark.smoke
-def test_category_note():
+# Fixture to create test categories
+@pytest.fixture
+def create_test_category(api_client, base_url="https://practice.expandtesting.com/notes/api/api-docs/#/"):
+    category_data = {"name": "Test Category"}
+    response = api_client.post(f" {base_url}/notes", json=category_data)
+    category_id = response.json()["id"]
+    yield category_id
 
-# Get the note ID
-    note_id = response.json()["id"]
-
-# Create a new category
-response = requests.post(
-    Base_URL + "/notes",
-    json={"name": "Test category"},
-)
-assert response.status_code == 200
-
-# Get the category ID
-category_id = response.json()["user_id"]
-
-# Categorizing the note
-response = requests.get(Base_URL + f"/notes/{id}")
-assert response.status_code == 200
-
-# Smoke test for updating a note
-@pytest.mark.smoke
-def test_update_note():
-    response = requests.put(
-        Base_URL + f"/notes{id}",
-        json={"title": "Updated note", "content": "This is an updated note."},
-    )
+# Fixture to update a note
+def test_update_note(api_client, create_test_note, base_url="https://practice.expandtesting.com/notes/api/api-docs/#/"):
+    note_id = create_test_note
+    updated_data = {
+        "title": "Updated Note",
+        "content": "This note has been updated."
+    }
+    response = api_client.put(f" {base_url}/notes/{id}", json=updated_data)
     assert response.status_code == 200
 
-# Smoke test for deleting a note
-@pytest.mark.smoke
-def test_delete_note():
-    response = requests.delete(Base_URL + f"/notes{id}")
+# Fixture to delete a note
+def test_delete_note(api_client, create_test_note, base_url="https://practice.expandtesting.com/notes/api/api-docs/#/"):
+    note_id = create_test_note
+    response = api_client.delete(f"{base_url}/notes/{id}")
     assert response.status_code == 200
 
-#End of smoke testing
+# Fixture to Search notes by title or category
+def test_search_note_by_title(api_client, create_test_note, base_url="https://practice.expandtesting.com/notes/api/api-docs/#/"):
+    note_id = create_test_note
+    response = api_client.get(f" {base_url}/notes?title=Test Note")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
 
-# Regression test for creating a note with an empty title
-@pytest.mark.regression
-def test_create_note_with_empty_title():
-    response = requests.post(
-        Base_URL + "/notes",
-        json={"content": "This is a test note."},
-    )
-    assert response.status_code == 400
-
-# Regression test for creating a note with an empty content
-@pytest.mark.regression
-def test_create_note_with_empty_content():
-    response = requests.post(
-        Base_URL + "/notes",
-        json={"title": "Test note"},
-    )
-    assert response.status_code == 400
-
-# Regression test for retrieving a note with an invalid ID
-@pytest.mark.regression
-def test_get_note_with_invalid_id():
-    response = requests.get(Base_URL + f"/notes{id}")
-    assert response.status_code == 404
-
-# Regression test for updating a note with an invalid ID
-@pytest.mark.regression
-def test_update_note_with_invalid_id():
-    response = requests.put(
-        Base_URL + f"/notes{id}",
-        json={"title": "Updated note", "content": "This is an updated note."},
-    )
-    assert response.status_code == 404
-
-# Regression test for deleting a note with an invalid ID
-@pytest.mark.regression
-def test_delete_note_with_invalid_id():
-    response = requests.delete(Base_URL + f"/notes/{id}")
-    assert response.status_code == 404
-
+def test_search_note_by_category(api_client, create_test_category, base_url="https://practice.expandtesting.com/notes/api/api-docs/#/"):
+    category_id = create_test_category
+    response = api_client.get(f" {base_url}/notes?category_id={category_id}")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
